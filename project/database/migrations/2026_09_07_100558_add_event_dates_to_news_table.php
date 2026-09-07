229cb0528abf17f5e4511416b411baa93a63d499
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2026-09-07 18:01:34 +0100

    Importa a facade DB e limpa a formatação da migração

--- project/database/migrations/2026_09_07_100558_add_event_dates_to_news_table.php
+++ project/database/migrations/2026_09_07_100558_add_event_dates_to_news_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -26,7 +27,6 @@ return new class extends Migration
         Schema::table('news', function (Blueprint $table) {
             $table->date('event_start_date')->nullable(false)->change();
         });
-
     }
 
     /**
