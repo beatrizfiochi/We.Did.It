872b8efabac136ea797bb979bf8d5dca0ef911b6
Beatriz Fiochi Soares da Silva <bfiochi-@c1r8s5.42porto.com>
2026-09-07 20:12:49 +0100

    Remove a eliminação da própria conta no Perfil (SCRUM-146)

--- project/routes/web.php
+++ project/routes/web.php
@@ -41,7 +41,6 @@ Route::post('/testemunhos', [TestimonialSubmissionController::class, 'store'])
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
